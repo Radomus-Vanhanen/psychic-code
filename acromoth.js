e19a01410f6b2ed62ceb34d92ecd7cc370046730
Radomus <58516041+Radomus-Vanhanen@users.noreply.github.com>
2020-12-05 01:10:10 -0800

    Add files via upload

--- acromoth.js
+++ acromoth.js
@@ -6,21 +6,22 @@ client.once('ready', () => {
 });
 
 // COMMAND
+client.on("message", message => {
+	if (message.content.startsWith(prefix + "mdm")) {
+		const args = message.content.split(" ");
+		const roleArgs = args.slice(0, 1);
+		const messageArgs = args.slice(1)
 
-if (message.content.startsWith(prefix + "mdm")) {
-	const args = message.content.split(" ");
-	const roleArgs = args.slice(0, 1);
-	const messageArgs = args.slice(1)
+		const role = message.guild.roles.find(role => role.name.toLowerCase('messager') === roleArgs.join(" ").toLowerCase())
+		if (!role) return message.reply('There is not such a role!');
 
-	const role = message.guild.roles.find(role => role.name.toLowerCase('messager') === roleArgs.join(" ").toLowerCase())
-	if (!role) return message.reply('There is not such a role!');
-
-	for (let i = 0; i < message.guild.members.size; i++) {
-		if (message.guild.members[i].roles.has(role.id)) {
-			message.guild.members[i].user.send(messageArgs.join(" "))
+		for (let i = 0; i < message.guild.members.size; i++) {
+			if (message.guild.members[i].roles.has(role.id)) {
+				message.guild.members[i].user.send(messageArgs.join(" "))
+			}
 		}
 	}
-}
+})
 
 client.login('Njc1MjMwMjI3MjQ4NTc4NjAx.Xj0HJw.ze8j4q4em9RjbcIVrJ1avd3z0fg');
 
